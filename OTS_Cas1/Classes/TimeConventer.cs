d37c21fcfa6bd74ec65a5906134c2f99eb4b288b
Andrej Katin <katin.andrej96@gmail.com>
2022-03-16 12:43:19 +0100

    Add project files.

---
+++ OTS_Cas1/Classes/TimeConventer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS_Cas1
+{
+    public class TimeConventer : ITimeConvert
+    {
+        public double ConvertHours(double days)
+        {
+            return days * 24;
+        }
+
+        public double ConvertMinutes(double days)
+        {
+            return days * 24 * 60;
+        }
+
+        public double ConvertSeconds(double days)
+        {
+            double temp = 1;
+            for (int i = 0; i < 2; i++)
+            {
+                temp = temp * 60;
+            }
+            return days * 24 * temp;
+        }
+    }
+}
